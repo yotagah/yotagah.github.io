b1e1e6598299c28d02bafcec6eb7b08f4f12e1c9
Jonathan Henning <jhon.hnng@gmail.com>
2025-01-06 18:14:22 -0300

    Add files via upload

---
+++ tictactoe3d/precache-manifest.352a790a25094445f19141be80794adb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199429308c6c9eadf4402549134d0044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a7a7bfa232e0de0104e",
+    "url": "/static/css/main.be6b95a7.chunk.css"
+  },
+  {
+    "revision": "4c1df2750292cd6fccb7",
+    "url": "/static/js/2.27bf4429.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.27bf4429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7a7bfa232e0de0104e",
+    "url": "/static/js/main.321f076b.chunk.js"
+  },
+  {
+    "revision": "d4ce418fefaf8c564405",
+    "url": "/static/js/runtime-main.63c81e9c.js"
+  }
+]);
